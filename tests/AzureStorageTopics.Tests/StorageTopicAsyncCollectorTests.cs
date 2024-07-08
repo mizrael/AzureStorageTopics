aea80547e4b3c8ee8513a2c6899998710c035887
David Guida <1432872+mizrael@users.noreply.github.com>
2024-07-09 00:25:36 +0200

    added more tests

--- tests/AzureStorageTopics.Tests/StorageTopicAsyncCollectorTests.cs
+++ tests/AzureStorageTopics.Tests/StorageTopicAsyncCollectorTests.cs
@@ -33,5 +33,70 @@ namespace AzureStorageTopics.Tests
             await sub.Received(1)
                 .SendMessageAsync("lorem ipsum", Arg.Any<CancellationToken>());
         }
+
+        [Fact]
+        public async Task AddAsync_should_enqueue_message_to_all_registered_subscriptions()
+        {
+            var attribute = new StorageTopicAttribute("mytopic");
+
+            var subscriptions = new[]
+            {
+                Substitute.ForPartsOf<QueueClient>(),
+                Substitute.ForPartsOf<QueueClient>(),
+                Substitute.ForPartsOf<QueueClient>(),
+                Substitute.ForPartsOf<QueueClient>()
+            };
+            foreach(var sub in subscriptions)
+                sub.WhenForAnyArgs(s => s.SendMessageAsync(default, default))
+                    .DoNotCallBase();
+
+            var subscriptionsProvider = Substitute.For<ISubscriptionsProvider>();
+            subscriptionsProvider.GetSubscriptionsAsync(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<CancellationToken>())
+                                 .Returns(subscriptions);
+
+            var connectionStringProvider = Substitute.For<IConnectionStringProvider>();
+            connectionStringProvider.GetConnectionString(Arg.Any<string>()).Returns("connectionString");
+
+            var sut = new StorageTopicAsyncCollector(attribute, subscriptionsProvider, connectionStringProvider);
+            await sut.AddAsync("lorem ipsum");
+
+            foreach (var sub in subscriptions)
+                await sub.Received(1)
+                         .SendMessageAsync("lorem ipsum", Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task AddAsync_should_not_fail_when_at_least_one_error_occurs()
+        {
+            var attribute = new StorageTopicAttribute("mytopic");
+
+            var subscriptions = new[]
+            {
+                Substitute.ForPartsOf<QueueClient>(),
+                Substitute.ForPartsOf<QueueClient>(),
+                Substitute.ForPartsOf<QueueClient>(),
+                Substitute.ForPartsOf<QueueClient>()
+            };
+            foreach (var sub in subscriptions)
+                sub.WhenForAnyArgs(s => s.SendMessageAsync(default, default))
+                    .DoNotCallBase();
+
+            subscriptions[1].WhenForAnyArgs(s => s.SendMessageAsync(default, default))
+                            .Throw(new Exception("error"));
+
+            var subscriptionsProvider = Substitute.For<ISubscriptionsProvider>();
+            subscriptionsProvider.GetSubscriptionsAsync(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<CancellationToken>())
+                                 .Returns(subscriptions);
+
+            var connectionStringProvider = Substitute.For<IConnectionStringProvider>();
+            connectionStringProvider.GetConnectionString(Arg.Any<string>()).Returns("connectionString");
+
+            var sut = new StorageTopicAsyncCollector(attribute, subscriptionsProvider, connectionStringProvider);
+            await sut.AddAsync("lorem ipsum");
+
+            foreach (var sub in subscriptions)
+                await sub.Received(1)
+                         .SendMessageAsync("lorem ipsum", Arg.Any<CancellationToken>());
+        }
     }
 }
